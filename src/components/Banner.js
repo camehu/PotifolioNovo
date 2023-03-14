90eb467b1bbe546f9bf83b77ac69471d38c0be61
camehu <camehu@gmail.com>
2023-03-14 13:54:43 -0300

    up_banner_resposive

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,27 +4,33 @@ import Button from "../components/Button";
 const Banner = () => {
   return (
     <>
-      <div class="row">
-        <div class="col-sm-5">
-          <img
-            src="https://www.threadscript.com/images/department/ZkoTXjGauq1aiiAUIlB52hkW1vPHj6gg.jpg"
-            alt="Banner Dev"
-            style={{ width: "650px", padding: "1rem", marginTop: "3rem" }}
-            class="rounded-pill"
-          />
-        </div>
+      <div class="d-flex flex-wrap justify-content-center">
+          <div class="p-2"> 
+              <img
+                src="https://www.threadscript.com/images/department/ZkoTXjGauq1aiiAUIlB52hkW1vPHj6gg.jpg"
+                alt="Banner Dev"
+                style={{ width: "550px", padding: "1rem", marginTop: "3rem" }}
+                class="rounded-pill"
+              />
+          </div>
+        
+      
+          <div class="p-2">
 
-        <div class="col-sm">
-          <div id="Titulo" style={{ padding: "10rem" }}>
-            <h1>Desenvolvedor Python</h1>
+            <div id="Titulo">
+            
+           <h1>Desenvolvedor Python</h1>
             <p>
               <h4> React | TypeScript | Flask | FastAPI | Django </h4>
               <Button />
             </p>
+            
           </div>
+          </div>
+        
         </div>
-      </div>
-    </>
+          
+          </>
   );
 };
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,49 +1,68 @@
 import React from "react";
 
-const Menu = () => {
+const Menu2 = () => {
   return (
     <>
-      <nav
-        class="navbar navbar-expand-sm bg-light"
-        style={{ position: "fixed", width: "100%" }}
-      >
+      <nav class="navbar navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            <img
-              src="https://wallup.net/wp-content/uploads/2019/09/261374-speed-racer-action-family-sport-race-cartoon-race-racing-56.jpg"
-              alt="Avatar Logo"
-              style={{ width: "50px" }}
-              class="rounded-pill"
-            />
+            Carlos Humberto
           </a>
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a className="nav-link" href="#Home">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a className="nav-link" href="#sobre">
-                Sobre mim
-              </a>
-            </li>
-
-            <li className="nav-item">
-              <a className="nav-link" href="#projetos">
-                Projetos
-              </a>
-            </li>
-
-            <li className="nav-item">
-              <a className="nav-link" href="#contato">
-                Contato
-              </a>
-            </li>
-          </ul>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasDarkNavbar"
+            aria-controls="offcanvasDarkNavbar"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="offcanvas offcanvas-end text-bg-dark"
+            tabindex="-1"
+            id="offcanvasDarkNavbar"
+            aria-labelledby="offcanvasDarkNavbarLabel"
+          >
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                Menu
+              </h5>
+              <button
+                type="button"
+                class="btn-close btn-close-white"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#sobre">
+                    Sobre Min
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#projetos">
+                    Projetos
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#contato">
+                    Contato
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </nav>
     </>
   );
 };
 
-export default Menu;
+export default Menu2;
