48788dbcbc68a5aa74564aa38c4363834bc770c0
camehu <camehu@gmail.com>
2023-03-14 20:52:43 -0300

    color_sobre_mim

--- src/components/SobreMim.js
+++ src/components/SobreMim.js
@@ -6,7 +6,7 @@ const Menu = () => {
       <div
         id="sobre"
         class="row bg-light p-3 flex-wrap"
-        style={{ width: "100%" }}
+        style={{ width: "auto" , backgraound-color:"Black"}}
       >
         <h2 style={{ padding: "20px" }}>Sobre Mim</h2>
         <div class="col-sm-6">
