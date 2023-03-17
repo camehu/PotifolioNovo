13eb0927f67a62732dfcc31b7ec96b046d8c5aa0
camehu <camehu@gmail.com>
2023-03-17 09:42:58 -0300

    sobreMim-container-img

--- src/components/SobreMim.js
+++ src/components/SobreMim.js
@@ -5,28 +5,28 @@ const Menu = () => {
     <>
       <div
         id="sobre"
-        class="row bg-primary p-5 flex-wrap"
+        class="row bg-primary p-5 mt-5 flex-wrap"
         style={{ width: "auto" }}
       >
-        <h2 style={{ padding: "20px" }}>Sobre Mim</h2>
+        <h1 style={{ padding: "13px" }}>Sobre Mim</h1>
 
         <div class="col-sm">
-          <p style={{ textAlign: "justify", margin: "30px" }}>
+          <p style={{  margin: "15px" }}>
             Graduado em Analise e Desenvolvidmento de sistemas pela Universidade
             Paulista(UNIP). Possuo Habilidade com os frameworks Flask, FastAPI,
             Django e Rest Framework. Possuo conhecimento em Desenvolvimento
             front-end React, TypeScript, HTML, CSS
           </p>
         </div>
-        <div class="col-sm">
+        <div class="col">
           <div class="container-fluid">
             <a class="navbar-brand" href="#sobreMim">
               <img
                 id="imgSobre"
                 src="https://wallup.net/wp-content/uploads/2019/09/261374-speed-racer-action-family-sport-race-cartoon-race-racing-56.jpg"
                 alt="Avatar Logo"
-                style={{ width: "350px" }}
-                class="rounded-pill"
+                style={{ width: "380px" }}
+                class="img-fluid"
               />
             </a>
           </div>
